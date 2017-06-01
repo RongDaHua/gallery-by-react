b5849595cc3d0e46893c37fd4abfb4e5b32e140f
RongDaHua <399670953@qq.com>
2017-06-01 17:09:54 +0800

    init project structure

---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+
+import React from 'react';
+
+//let yeomanImage = require('../images/yeoman.png');
+
+//获取图片数据，将图片名信息转成URL路径信息
+let imageDatas=require('../data/imageDatas.json');
+
+function getImageURL(imageDatasArr){
+	for (var i = 0,j=imageDatasArr.length; i <j; i++) {
+		var singleImageData=imageDatasArr[i];
+		singleImageData.imageURL=require('../images/'+singleImageData.fileName);
+		imageDatasArr[i]=singleImageData;
+	}
+	return imageDatasArr;
+}
+
+imageDatas=getImageURL(imageDatas);
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+    	<section className="stage">
+    		<section className="img-sec">
+    		</section>
+    		<nav className="controller-nav">
+    		</nav>
+    	</section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
