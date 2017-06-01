2c5493674589d7f620d58faac4402396727f6539
RongDaHua <399670953@qq.com>
2017-06-01 22:49:43 +0800

    change

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -42,7 +42,7 @@ function getDefaultModules() {
       },
       {
         test: /\.styl/,
-        loader: 'style-loader!css-loader!stylus-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version","firefox 15"]}!styls-loader'
       },
       {
         test: /\.json$/,
